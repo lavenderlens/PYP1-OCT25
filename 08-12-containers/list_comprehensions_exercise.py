0b4caf591ed99756aaea6dea377074f6a5c688d2
Alan Lavender <alanlavendersmv@gmail.com>
2025-10-23 11:00:55 +0100

    list-comprehensions exercises

---
+++ 08-12-containers/list_comprehensions_exercise.py
@@ -0,0 +1,14 @@
+names = ["Ellis", "Brad", "Katy", "Becca", "Shaun", "Chris", "Owen", "Mike", "Bertrand", "Immanuel", "Ludwig", "Alan", "Zak"]
+
+# using list comprehensions,
+# tell me
+
+# 1. the names of less than 5 characters length
+
+# 2. the names of greater than 5 characters length
+
+# 3. the names with even numbers of characters
+
+# 4. the names beginning with "B" - case-insensitive, use startswith() method of str()
+
+# 5. all the names, upper-cased
--- 08-12-containers/list_exercise.py
+++ 08-12-containers/list_exercise.py
@@ -1,27 +1,4 @@
-'''found in manual P 127 0r 8-19
-Lists
-In this exercise you will create a script that emulates a sporting league table. Teams will
-move up and down the table, some will be relegated and some will be promoted. The
-team at index 0 (zero) is to be considered on top of the table.
-1. 2. Create a script named ch8_lists.py.
-Declare a variable named table and assign it a list of teams (strings) as follows:
-Bath, Derby, Gloucester, Lancaster, Newcastle, Plymouth, Salford, and Wakefield.
-3. 4. 6. 7. 8. 9. Move Newcastle into 3rd position (remove and then insert).
-Move Derby into 4th position.
-5. Relegate (remove) Salford.
-Promote (append) Canterbury at the bottom of the table.
-In one statement, relegate Plymouth and promote York (update).
-Extract the top two teams into a new list named to_play_in_europe.
-If Newcastle is to play in Europe, print Newcastle to play in Europe to the
-console.
-10. Code a loop that iterates over the table.
-11. Inside the loop print each team to the console. The output should be as follows:
-Bath
-Newcastle
-Gloucester
-Derby
-Lancaster
-York
-Wakefield
-Canterbury
+'''found in manual P 127 
+0r 8-19
+Or screenshot in this folder
 '''
